26dfabb5e5782dcd1abd6944bb5d559309bc2303
php 11 <php.11@genio.cec.ime.usp.br>
2018-01-22 20:19:22 -0200

    adicao classe cliente

---
+++ Cliente.php
@@ -0,0 +1,16 @@
+<?php
+class Cliente {
+  public $nome;
+  public $codigo;
+}
+
+
+$c = new Cliente();
+$c->nome = "Rafael Constantino";
+$c->codigo = 1;
+echo "nome: $c->nome código = $c->codigo".PHP_EOL;
+
+$c2 = new Cliente();
+$c2->nome = "Jonas Hirata";
+$c2->codigo = 2;
+echo "nome: $c2->nome código = $c2->codigo".PHP_EOL;
