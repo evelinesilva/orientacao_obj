6cc80910d0d75d3510c54154d5a5257245bee93f
php 11 <php.11@genio.cec.ime.usp.br>
2018-01-22 22:26:05 -0200

    Classe Cliente

--- Cliente.php
+++ Cliente.php
@@ -1,11 +1,12 @@
 <?php
 class Cliente {
   public $nome;
+  public $email;
   public $codigo;
 }
 
 
-$c = new Cliente();
+/*$c = new Cliente();
 $c->nome = "Rafael Constantino";
 $c->codigo = 1;
 echo "nome: $c->nome código = $c->codigo".PHP_EOL;
@@ -14,3 +15,4 @@ $c2 = new Cliente();
 $c2->nome = "Jonas Hirata";
 $c2->codigo = 2;
 echo "nome: $c2->nome código = $c2->codigo".PHP_EOL;
+*/
